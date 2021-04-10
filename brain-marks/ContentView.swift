44940e141733c1b349a2cea7d3a88fb1ce6a16e8
Mikaela Caron <mikaelacaron@gmail.com>
2021-04-10 16:24:12 -0400

    Initial Commit
    New idea

---
+++ brain-marks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  brain-marks
+//
+//  Created by Mikaela Caron on 4/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ brain-marks/brain_marksApp.swift
@@ -0,0 +1,17 @@
+//
+//  brain_marksApp.swift
+//  brain-marks
+//
+//  Created by Mikaela Caron on 4/10/21.
+//
+
+import SwiftUI
+
+@main
+struct brain_marksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
