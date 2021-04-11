f5da6c62bd96873ce2d11c3d7204da00cf60edb2
Shloak Aggarwal <31565271+shloak17107@users.noreply.github.com>
2021-04-11 20:55:28 +0300

    Remove get url function from TweetList

--- brain-marks/TweetList.swift
+++ brain-marks/TweetList.swift
@@ -25,51 +25,5 @@ struct TweetList: View {
                 }
             }
         }
-        
-        Spacer()
-    }
-}
-
-extension TweetList {
-    func get(url:String) {
-        var count = 0
-
-        let id = url.components(separatedBy: "/").last!.components(separatedBy: "?")[0]
-        var request = URLRequest(url: URL(string: "https://api.twitter.com/2/tweets/\(id)")!,timeoutInterval: Double.infinity)
-        
-        request.addValue("Bearer \(Secrets.bearerToken)", forHTTPHeaderField: "Authorization")
-        
-        request.httpMethod = "GET"
-        
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data else {
-                print(String(describing: error))
-                
-                return
-            }
-            
-            if let response = response as? HTTPURLResponse {
-                guard (200 ... 299) ~= response.statusCode else {
-                    print("Status code :- \(response.statusCode)")
-                    return
-                }
-                
-                do {
-                    if error == nil {
-                        let result = try JSONDecoder().decode([ResponseModel].self, from: data)
-                        // update UI
-                        count += 1
-                        print("\(count)\(result)")
-
-                    }
-                    DispatchQueue.main.async {
-                        // update UI
-                    }       
-                } catch {
-                    print("\(error.localizedDescription)")
-                }
-            }
-        }
-        task.resume()
     }
 }
