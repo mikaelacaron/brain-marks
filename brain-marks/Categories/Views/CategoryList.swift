7a7df64c6732b56352620ba0deee4717e2ec7882
PRABAL4546 <prabaljitwalia4546@gmail.com>
2021-05-29 22:59:12 +0530

    separated newCategorySheetView

--- brain-marks/Categories/Views/CategoryList.swift
+++ brain-marks/Categories/Views/CategoryList.swift
@@ -54,54 +54,6 @@ struct CategoryList: View {
     }
 }
 
-struct NewCategorySheetView: View {
-    @Environment(\.presentationMode) var presentationMode
-    @State var newCategory = ""
-    
-    var body: some View {
-        NavigationView {
-            VStack {
-                TextField("Enter name of new category", text: $newCategory)
-                    .foregroundColor(.gray)
-                
-                
-                Spacer()
-                
-                HStack(spacing: 25) {
-                    Button {
-                        presentationMode.wrappedValue.dismiss()
-                    } label: {
-                        Text("Cancel")
-                            .frame(width: 150, height: 50)
-                            .foregroundColor(.white)
-                            .background(Color(UIColor(named: "twitter")!))
-                            .font(.system(size: 20, weight: .semibold, design: .default))
-                            .cornerRadius(10)
-                    }
-                    
-                    Button {
-                        presentationMode.wrappedValue.dismiss()
-                        DataStoreManger.shared.createCategory(
-                            category: AWSCategory(name: newCategory,
-                                                  imageName: "swift"))
-                    } label: {
-                        Text("Create")
-                            .frame(width: 150, height: 50)
-                            .foregroundColor(.white)
-                            .background(Color(UIColor(named: "twitter")!))
-                            .font(.system(size: 20, weight: .semibold, design: .default))
-                            .cornerRadius(10)
-                    }
-                }
-                .padding(20)
-            }
-            .navigationBarTitle("New Category")
-            .sheet(isPresented:$showAddURLView) {
-            AddURLView(categories: viewModel.categories)
-        }
-    }
-}
-
 struct CategoryList_Previews: PreviewProvider {
     static var previews: some View {
         CategoryList()
