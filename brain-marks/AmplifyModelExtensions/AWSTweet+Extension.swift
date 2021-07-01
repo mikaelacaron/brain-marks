883280017ea2acee81c4d8b6064f407fdcdcb08d
PRABAL4546 <prabaljitwalia4546@gmail.com>
2021-07-01 14:16:48 +0530

    mandatory category selection working

--- brain-marks/AmplifyModelExtensions/AWSTweet+Extension.swift
+++ brain-marks/AmplifyModelExtensions/AWSTweet+Extension.swift
@@ -8,3 +8,12 @@
 import Foundation
 
 extension AWSTweet: Identifiable {}
+extension AWSTweet:Hashable{
+    public static func == (lhs: AWSTweet, rhs: AWSTweet) -> Bool {
+        return lhs.id == rhs.id
+    }
+
+    public func hash(into hasher: inout Hasher) {
+             hasher.combine(id)
+         }
+}
