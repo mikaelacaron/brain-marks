6788d38007bafd0f4e6f3ad0d91936d9f1b76ee8
Mikaela Caron <mikaelacaron@gmail.com>
2021-09-30 18:23:56 -0400

    Add example Secrets file

---
+++ brain-marks/Secrets-Example.swift
@@ -0,0 +1,14 @@
+//
+//  Secrets-Example.swift
+//  brain-marks
+//
+//  Created by Mikaela Caron on 9/30/21.
+//
+
+import Foundation
+
+// swiftlint:disable line_length
+
+enum SecretsExample {
+    static let bearerToken = ""
+}
