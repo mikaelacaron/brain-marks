ac24085afd98b2f60f72ed94725d2e03842e5c7d
Mikaela Caron <mikaelacaron@gmail.com>
2022-05-13 09:38:42 -0400

    Create JSONDataManager

---
+++ brain-marks/Managers/JSONDataManager.swift
@@ -0,0 +1,14 @@
+//
+//  JSONDataManager.swift
+//  brain-marks
+//
+//  Created by Mikaela Caron on 5/13/22.
+//
+
+import Foundation
+
+final class JSONDataManager {
+    private init() { }
+    
+    static let shared = JSONDataManager()
+}
