54f94bcacd9e06a9f693875e5d1e745dab2ba115
Jay <heyjay@cctplus.dev>
2023-01-20 21:43:13 -0600

    forgot to save these 🤦

--- brain-marks/CoreData/StorageProvider.swift
+++ brain-marks/CoreData/StorageProvider.swift
@@ -32,6 +32,14 @@ final class StorageProvider {
                 category.addToTweets(tweet)
             }
         }
+        do {
+            try controller.context.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
         return controller
     }()
 
